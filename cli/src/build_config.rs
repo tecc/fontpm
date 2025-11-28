54c09e7c799460d36c55e883fbeb4d5895e7245c
tecc <tecc@tecc.me>
2025-11-28 05:45:22 +0100

    refactor(next)!: Remove old crates

--- cli/src/build_config.rs
+++
@@ -1,9 +0,0 @@
-pub fn sources() -> Vec<String> {
-    let mut vec: Vec<String> = vec!();
-
-    // Now here comes the fun part!
-    #[cfg(feature = "google-fonts")]
-    vec.push(fontpm_source_google_fonts::GoogleFontsSource::ID.into());
-
-    vec
-}
